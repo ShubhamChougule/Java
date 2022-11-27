439d9493704caec434a17c8a2453e7cd122ea703
Shubham Sanjay Chougule <52528575+ShubhamChougule@users.noreply.github.com>
2022-11-27 07:37:51 +0530

    Add files via upload

---
+++ Others/Que11A.java
@@ -0,0 +1,17 @@
+class Que11A {
+    public static void main(String[] shubham) {
+        // implicit casting
+        
+        // int to long
+        int A = 1023;
+        long B = A;
+        System.out.println("int : " + A);
+        System.out.println("long : " + B);
+
+        // float to double
+        float X = 12.33f;
+        double Y = X;
+        System.out.println("float : " + X);
+        System.out.println("double : " + Y);
+    }
+}
